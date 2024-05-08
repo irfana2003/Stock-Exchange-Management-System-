35fc851920ec9630eb930b563639c5543fca2dbb
irfana2003 <2100032205@kluniversity.in>
2024-05-08 23:20:47 +0530

    Initial

---
+++ js/main.js
@@ -0,0 +1,150 @@
+$(document).ready(function(){
+
+
+    $('.fa-bars').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+    });
+  
+    $(window).on('load scroll',function(){
+      $('.fa-bars').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+  
+      if($(window).scrollTop() > 30){
+        $('.header').css({'background':'#6C5CE7','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+      }else{
+        $('.header').css({'background':'none','box-shadow':'none'});
+      }
+    });
+  
+  
+    $('.accordion-header').click(function(){
+      $('.accordion .accordion-body').slideUp();
+      $(this).next('.accordion-body').slideDown();
+      $('.accordion .accordion-header span').text('+');
+      $(this).children('span').text('-');
+    });
+  
+  
+  
+  });
+
+
+  // SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+  series: [{
+    data: [10, 8, 6, 4, 2]
+  }],
+  chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+      show: false
+    },
+  },
+  colors: [
+    "#246dec",
+    "#cc3c43",
+    "#367952",
+    "#f5b74f",
+    "#4f35a1"
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%',
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+    categories: ["Premier Polyfil", "Sadbhav Engg", "Axita Cotton", "HPL Electric", "Excel Realty"],
+  },
+  yaxis: {
+    title: {
+      text: "Company"
+    }
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+
+// AREA CHART
+var areaChartOptions = {
+  series: [{
+    name: 'Buyers',
+    data: [31, 40, 28, 51, 42, 109, 100]
+  }, {
+    name: 'Sellers',
+    data: [11, 32, 45, 32, 34, 52, 41]
+  }],
+  chart: {
+    height: 350,
+    type: 'area',
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#4f35a1", "#246dec"],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  markers: {
+    size: 0
+  },
+  yaxis: [
+    {
+      title: {
+        text: 'Buyers',
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: 'Sellers',
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+  }
+};
+
+var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areaChart.render();
